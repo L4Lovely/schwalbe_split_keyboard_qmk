fbe6e199a431ab928433654fe2457aff98a8d8eb
OpenSorcerer <91776042+L4Lovely@users.noreply.github.com>
2026-01-10 08:05:06 +0100

    Comment out HAL configuration settings

--- halconf.h
+++ halconf.h
@@ -1,5 +1,5 @@
-#pragma once
-#define HAL_USE_SIO FALSE
-#define HAL_USE_SERIAL FALSE
-#define HAL_USE_UART FALSE
-#include_next <halconf.h>
+// #pragma once
+// #define HAL_USE_SIO FALSE
+// #define HAL_USE_SERIAL FALSE
+// #define HAL_USE_UART FALSE
+// #include_next <halconf.h>
